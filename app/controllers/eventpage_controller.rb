72db6a74ad4b7c0d897b661568d8c452264c0eba
sasabe109 <atsuc109@gmail.com>
2021-05-24 18:02:32 +0900

    ‘コミット#1’

---
+++ app/controllers/eventpage_controller.rb
@@ -0,0 +1,19 @@
+class EventpageController < ApplicationController
+    def make_event
+        @event=Event.new
+    end
+
+    def event_list            
+    end
+
+    def schedule      
+    end
+    
+    def event_list
+    end
+    
+    def event_create
+        
+    end
+
+end
