7d5e3ba84554f3cb194ac2211cf2afd72a992658
sasabe109 <atsuc109@gmail.com>
2021-05-28 15:32:20 +0900

    Eventsテーブルの削除に成功した

--- db/migrate/20210524075101_add_event_date_toevents.rb
+++
@@ -1,5 +0,0 @@
-class AddEventDateToevents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :Event_Date, :Date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_070623) do
-
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.integer "Date_number"
-    t.boolean "Event_type"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.date "Event_Date"
-  end
+ActiveRecord::Schema.define(version: 2021_05_18_054444) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
